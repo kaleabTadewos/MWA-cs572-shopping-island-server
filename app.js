4560fa0ac108998dff0fdfcfa71ba720f5f0ce3e
Ashenafi <ashenafi66@gmail.com>
2020-06-14 02:16:32 -0500

    added authorization rout and control

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const userRoutes = require('./routes/users');
+const loginRoutes = require('./routes/login');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(userRoutes);
+app.use(loginRoutes);
 
 mongoose.connect('mongodb://localhost:27017/Island-shopping', { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => {
