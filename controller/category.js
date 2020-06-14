c4b70708973db4083d1004ccd9ad25f421f419f2
kaleab <tadewoskaleab@gmail.com>
2020-06-14 17:03:01 -0500

    crud operations inside category controller

--- controller/category.js
+++ controller/category.js
@@ -1,53 +1,48 @@
-const {Category, validate} = require('../models/category'); 
+const { Category, validate } = require('../models/category');
 const ApiResponse = require('../models/api-response');
 
+//CRUD Operations
+//Create Operation
 exports.insert = async (req, res, next) => {
-    const { error } = validate(req.body);
-    if (error) return res.send(error.details[0].message);
-
-    const category = await Category.create(req.body);
-    if (!category) return res.send(new ApiResponse(500, 'error', err));
-    res.status(201).send(new ApiResponse(201, 'success', category));
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        const category = await Category.create(req.body);
+        res.status(201).send(new ApiResponse(201, 'success', category));
 };
 
-exports.getById = async (req, res, next) => {
-    const category = await Category.findById(req.params.id);
-    if(!category) return res.status(404).send(new ApiResponse(404 , 'error' , err));
-    res.status(200).send(new ApiResponse(200 , 'success' , category));
+//Retrive Operations
+exports.findById = async (req, res, next) => {
+        const category = await Category.findById(req.params.id);
+        if (!category) return res.status(404).send('category with the given ID was not found.');
+        res.status(200).send(new ApiResponse(200, 'success', category));
 };
 
-// exports.patchById = (req, res, next) => {
-//     User.findById(req.params.userId)
-//         .then(user => {
-//             for (let i in req.body) {
-//                 user[i] = req.body[i];
-//             }
-//             return user.save();
-//         })
-//         .then(result => {
-//             res.status(200).send(new ApiResponse(200, 'success', result));
-//         });
-// };
-
-exports.list = async (req, res, next) => {
-    try {
+exports.findAll = async (req, res, next) => {
         const categorys = await Category.find();
-        res.status(200).send(new ApiResponse(200 , 'success' , categorys));
-    }
-    catch (err){
-        return res.status(404).send(new ApiResponse(404 , 'error' , err));
-    } 
+        if (!categorys) return res.status(404).send('categorys not found.');
+        res.status(200).send(new ApiResponse(200, 'success', categorys));
 }
 
-// exports.removeById = (req, res, next) => {
-//     User.findByIdAndDelete(req.params.userId)
-//         .then(result => {
-//             res.status(200).send(new ApiResponse(200, 'success', result));
-//         })
-//         .catch(err => {
-//             res.status(500).send(new ApiResponse(500, 'error', err));
-//         });
-// };
+//Update Operation
+exports.updateById = async (req, res, next) => {
+       const { error } = validate(req.body);
+    console.log(error);
+       if (error) return res.status(400).send(error.details[0].message);
+        const category = await Category.findOneAndUpdate(req.params.id,
+            { 
+              name: req.body.name
+            }, { new: true });
+        console.log(category);
+        category.save();
+        res.status(200).send(new ApiResponse(200, 'success', category));
+};
+
+//Delete Operation
+exports.removeById = async (req, res, next) => {
+        const category = await Category.findByIdAndRemove(req.params.id);
+        if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+        res.status(200).send(new ApiResponse(200, 'success', category));
+};
 
 // exports.getNoOfUsersInRole = (req, res, next) => {
 //     User.aggregate([
