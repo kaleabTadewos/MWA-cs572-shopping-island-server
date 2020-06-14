c6a79db6b813a663bcc4b0eaa56ab45a70750c03
Ashenafi <ashenafi66@gmail.com>
2020-06-13 22:21:26 -0500

    connected the app with Mongo and Node js server

--- app.js
+++ app.js
@@ -26,22 +26,29 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-module.exports = app;
+//module.exports = app;
 
-mongoConnect(() => {
-  app.listen(3000);
-})
+// mongoConnect(() => {
+//   app.listen(3000);
+// })
+
+mongoose.connect('mongodb://localhost:27017/Island-shopping', { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("server is running on 3000 ...");
+        })
+    }).catch((err) => console.log(err));
---
+++ models/User.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    status: {
+        type: String,
+        enum: ['Active', 'Pending', 'Inactive'],
+        default: 'Pending'
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Seller', 'Buyer']
+    }
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(255).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(1024).required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
