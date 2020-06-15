baa8d620c51c701afd60e87fe45d8f49021042d1
kaleab <tadewoskaleab@gmail.com>
2020-06-15 17:11:42 -0500

    finalize subcategory api

--- controller/subCategory.controller.js
+++ controller/subCategory.controller.js
@@ -12,25 +12,25 @@ exports.insert = async (req, res, next) => {
     const category = await Category.findById(req.body.categoryId);
     if(!category) res.status(400).send(new ErrorResponse('400' , 'Invalid Category Id!'));
  
-    let newSubCategory = new SubCategoryy({
+    let newSubCategory = new SubCategory({
         name: req.body.name , 
         category : category
     });
-    const subCategoryy = await SubCategoryy.create(newSubCategory);
-    res.status(201).send(new ApiResponse(201, 'success', subCategoryy));
+    const subCategory = await SubCategory.create(newSubCategory);
+    res.status(201).send(new ApiResponse(201, 'success', subCategory));
 };
 
 //Retrive Operations
 exports.findById = async (req, res, next) => {
     const { error } = validateId({_id:req.params.id});
     if (error) return res.status(400).send(new ErrorResponse('400' , error.details[0].message));
-    const subCategoryy = await SubCategoryy.findById(req.params.id);
-    if (!subCategoryy) return res.status(404).send(new ErrorResponse('400' , 'no content found!'));
-    res.status(200).send(new ApiResponse(200, 'success', subCategoryy));
+    const subCategory = await SubCategory.findById(req.params.id);
+    if (!subCategory) return res.status(404).send(new ErrorResponse('400' , 'no content found!'));
+    res.status(200).send(new ApiResponse(200, 'success', subCategory));
 };
 
 exports.findAll = async (req, res, next) => {
-    const subCategories = await SubCategoryy.find();
+    const subCategories = await SubCategory.find();
     if (!subCategories) return res.status(404).send(new ErrorResponse('400' , 'no content found!'));
     res.status(200).send(new ApiResponse(200, 'success', subCategories));
 }
@@ -41,23 +41,23 @@ exports.updateById = async (req, res, next) => {
     if (error) return res.status(400).send(new ErrorResponse('400' , error.details[0].message));
     const category = await Category.findById(req.body.categoryId);
     if(!category) res.status(400).send(new ErrorResponse('400' , 'Invalid Category Id!'));
-    const subCategoryy = await SubCategoryy.findOneAndUpdate(req.params.id,
+    const subCategory = await SubCategory.findOneAndUpdate(req.params.id,
         {
             name: req.body.name , 
             category: category
         }, 
         { new: true, useFindAndModify: true });
-    subCategoryy.save();
-    res.status(200).send(new ApiResponse(200, 'success', subCategoryy));
+    subCategory.save();
+    res.status(200).send(new ApiResponse(200, 'success', subCategory));
 };
 
 //Delete Operation
 exports.removeById = async (req, res, next) => {
     const { error } = validateId({_id:req.params.id});
     if (error) return res.status(400).send(new ErrorResponse('400' , error.details[0].message));
-    const subCategoryy = await SubCategoryy.findByIdAndRemove(req.params.id);
-    if (!subCategoryy) return res.status(404).send(new ErrorResponse('400' , 'no content found!'));
-    res.status(200).send(new ApiResponse(200, 'success', subCategoryy));
+    const subCategory = await SubCategory.findByIdAndRemove(req.params.id);
+    if (!subCategory) return res.status(404).send(new ErrorResponse('400' , 'no content found!'));
+    res.status(200).send(new ApiResponse(200, 'success', subCategory));
 };
 
 // exports.getNoOfUsersInRole = (req, res, next) => {
