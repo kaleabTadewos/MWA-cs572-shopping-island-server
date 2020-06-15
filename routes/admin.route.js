3c61b1813fd8eb240d7815ada31078e286d3e86f
Ashenafi <ashenafi66@gmail.com>
2020-06-15 15:47:28 -0500

    added model and crud opetion for point calculator

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const unitController = require('../controller/unit.controller');
 const shippingAddressController = require('../controller/shippingAddress.controller');
+const pointCalculatorControler = require('../controller/pointCalculator.controller');
 const subCategoryController = require('../controller/subCategory.controller');
 
 /* units Routes */
@@ -24,6 +25,13 @@ router.post('/shipping-address', shippingAddressController.insert);
 router.put('/shipping-address/', shippingAddressController.updateById);
 router.get('/shipping-address/:id', shippingAddressController.findById);
 router.delete('/shipping-address/:id', shippingAddressController.removeById);
+
+/* Shipping Address Routes*/
+router.get('/point-calculator', pointCalculatorControler.findAll);
+router.post('/point-calculator', pointCalculatorControler.insert);
+router.put('/point-calculator', pointCalculatorControler.updateById);
+router.get('/point-calculator/:id', pointCalculatorControler.findById);
+router.delete('/point-calculator/:id', pointCalculatorControler.removeById);
 
 
 
