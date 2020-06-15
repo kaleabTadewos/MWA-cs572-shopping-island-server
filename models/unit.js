06270abdfb6c35b4f5b3d34e6b615330e1060889
Ashenafi <ashenafi66@gmail.com>
2020-06-14 22:08:48 -0500

    added unit model

---
+++ models/unit.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const unitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+});
+
+
+function validateUnit(unit) {
+    const schema = {
+        name: Joi.string().required(),
+        quantity: Joi.number().required(),
+    };
+
+    return Joi.validate(unit, schema);
+}
+
+exports.unit = mongoose.model('Unit', unitSchema);
+exports.validateUnit = validateUnit;
