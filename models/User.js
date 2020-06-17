60934b136b4b8cbbe8bde8bc2c79a9d695fc0333
ero kaleab tadewos <tadewoskaleab@gmail.com>
2020-06-17 17:11:25 -0500

    minor fixes for enum status

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['ACTIVE ', 'PENDING', 'INACTIVE'],
+        enum: ['ACTIVE', 'PENDING', 'INACTIVE'],
         default: 'PENDING'
     },
     role: {
