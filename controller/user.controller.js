9833aeb06315b0bf2e99b3fb6b6216f4fda84f9d
ero kaleab tadewos <tadewoskaleab@gmail.com>
2020-06-17 11:34:44 -0500

    add endpoint to place order

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,17 +8,17 @@ const { OrderDetail } = require('../models/orderDetail');
 const { Item } = require('../models/item');
 const ApiResponse = require('../models/apiResponse');
 const ErrorResponse = require('../models/errorResponse');
-const { validateId, validateWithOutId, validateWithId, validateShoppingCart, validateOrderPlacement } = require('../models/request/user.request');
+const { validateId, validateWithOutId, validateWithId, validateShoppingCart, validateOrderPlacement , validateSingleOrderPlacement} = require('../models/request/user.request');
 
 
-exports.insert = async(req, res, next) => {
+exports.insert = async (req, res, next) => {
     const { error } = validateWithOutId(req.body);
     if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
     let userExist = await User.findOne({ email: req.body.email });
     if (userExist) return res.status(400).send('user already exist');
 
     const newAddress = await Address.findById(req.body.addressId);
-
+    console.log(newAddress);
     if (!newAddress) return res.status(400).send('invalid address id!');
     let address = [newAddress];
     const user = await User.create({
@@ -44,7 +44,7 @@ exports.insert = async(req, res, next) => {
 };
 
 //Retrive Operations
-exports.findById = async(req, res, next) => {
+exports.findById = async (req, res, next) => {
     const { error } = validateId({ _id: req.params.id });
     if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
     const user = await User.findById(req.params.id);
@@ -52,14 +52,14 @@ exports.findById = async(req, res, next) => {
     res.status(200).send(new ApiResponse(200, 'success', user));
 };
 
-exports.findAll = async(req, res, next) => {
+exports.findAll = async (req, res, next) => {
     const user = await User.find();
     if (!user) return res.status(404).send(new ErrorResponse('400', 'no content found!'));
     res.status(200).send(new ApiResponse(200, 'success', user));
 }
 
 //Update Operation
-exports.updateById = async(req, res, next) => {
+exports.updateById = async (req, res, next) => {
     const { error } = validateWithId(req.body);
     if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
     //const subCategory = await SubCategory.findById(req.body.subCategoryId);
@@ -87,7 +87,7 @@ exports.updateById = async(req, res, next) => {
 };
 
 //Delete Operation
-exports.removeById = async(req, res, next) => {
+exports.removeById = async (req, res, next) => {
     const { error } = validateId({ _id: req.params.id });
     if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
     const user = await User.findByIdAndRemove(req.params.id);
@@ -95,7 +95,7 @@ exports.removeById = async(req, res, next) => {
     res.status(200).send(new ApiResponse(200, 'success', user));
 };
 
-exports.addToCart = async(req, res, next) => {
+exports.addToCart = async (req, res, next) => {
     const { error } = validateShoppingCart(req.body);
     if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
     const item = await Item.findById(req.body.itemId);
@@ -107,13 +107,11 @@ exports.addToCart = async(req, res, next) => {
     res.status(200).send(new ApiResponse(200, 'success', user.shoppingCart));
 };
 
-exports.placeOrder = async(req, res, next) => {
-    console.log("Negative");
-    // const { error } = validateOrderPlacement(req.body);
-    // if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
-    console.log("one");
+exports.placeOrder = async (req, res, next) => {
+    const { error } = validateOrderPlacement(req.body);
+    if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
 
-    let fncc = async function(reqItems) {
+    let fncc = async function (reqItems) {
         let items = [];
         for (let h = 0; h < reqItems.length; h++) {
             const item = await Item.findById(reqItems[h]);
@@ -135,26 +133,50 @@ exports.placeOrder = async(req, res, next) => {
     //     console.log("One inside");
     //     console.log(items);
     // });
-    console.log("two");
-    let newAddress = Address.findById(req.body.addressId);
-    let newAddresss = Address.findById(req.body.addressId);
+
+    const newAddress = await Address.findById(req.body.addressId);
+   // let newOrderDetail = items; 
+    let newOrder = {orderDetail : items , shippingAddress : newAddress , orderDate : Date.now()};
+
     // //console.log(items);
     const user = await User.findByIdAndUpdate(req.body.userId, {
-        'order.orderDetail': items,
-        'order.orderDate': Date.now(),
+        // 'order.orderDetail': items,
+        // 'order.orderDate': Date.now(),
         $addToSet: { addresses: newAddress },
-        'order.shippingAddress.state': newAddresss.state,
-        'order.shippingAddress.city': newAddresss.city,
-        'order.shippingAddress.zipCode': newAddresss.zipCode,
-        'order.shippingAddress.street': newAddresss.street,
-        'order.shippingAddress._id': newAddresss._id,
-        'order.shippingAddress.addressString': newAddresss.addressString
+        $push: {order: newOrder},
+        //order: newOrder 
+        // ,
+        // 'order.shippingAddress.state': newAddress.state,
+        // 'order.shippingAddress.city': newAddress.city,
+        // 'order.shippingAddress.zipCode': newAddress.zipCode,
+        // 'order.shippingAddress.street': newAddress.street,
+        // 'order.shippingAddress._id': newAddress._id,
+        // 'order.shippingAddress.addressString': newAddress.addressString
 
     }, { new: true, useFindAndModify: true });
     res.status(200).send(new ApiResponse(200, 'success', items));
 
 }
 
+exports.placeSingleOrder = async (req, res, next) => {
+    const { error } = validateSingleOrderPlacement(req.body);
+    if (error) return res.status(400).send(new ErrorResponse('400', error.details[0].message));
+
+    const item = [];
+    item[0] = await Item.findById(req.body.itemId);
+    if (!item) res.status(400).send(new ErrorResponse('400', 'no content found!'));
+
+    const newAddress = await Address.findById(req.body.addressId);
+    let newOrder = {orderDetail : item , shippingAddress : newAddress , orderDate : Date.now()};
+
+   const user = await User.findByIdAndUpdate(req.body.userId, {
+        $addToSet: { addresses: newAddress },
+        $push: {order: newOrder}
+
+    }, { new: true, useFindAndModify: true });
+    res.status(200).send(new ApiResponse(200, 'success', item));
+
+}
 /* get a user. */
 // exports.getUser = async function(request, response) {
 //         const user = await User.findById(request.user._id).select('-password');
